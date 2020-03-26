630a020589ddfe10719e912c6998de377a8c2145
azdoolittle <drewdoolittle@gmail.com>
2020-03-25 23:48:09 -0400

    Added Github link

--- src/App.js
+++ src/App.js
@@ -6,18 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Andrew Doolittle
+        Full Stack Software Engineer
+        <a href="https://github.com/azdoolittle"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Ei-sc-github.svg/512px-Ei-sc-github.svg.png"></img></a>
       </header>
     </div>
   );
