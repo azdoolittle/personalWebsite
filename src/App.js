a543c62426a1a0928463e2ae8478de3ce4b7480d
azdoolittle <drewdoolittle@gmail.com>
2020-03-26 23:13:26 -0400

    added nav to landing

--- src/App.js
+++ src/App.js
@@ -6,10 +6,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Andrew Doolittle
-        Full Stack Software Engineer
+        <div>
+          <h2>
+            Andrew Doolittle
+          </h2>
+        </div>
+        <div>
+          Full Stack Software Engineer
+        </div>
         <a href="https://github.com/azdoolittle"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Ei-sc-github.svg/512px-Ei-sc-github.svg.png"></img></a>
       </header>
+      <nav className="nav">
+        <div>About</div>
+        <div>Software Engineering Applications</div>
+        <div>Technologies</div>
+      </nav>
     </div>
   );
 }
