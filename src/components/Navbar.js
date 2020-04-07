9f67b98dc26914fc89aaa6a7b13e618401238162
azdoolittle <drewdoolittle@gmail.com>
2020-04-06 23:50:48 -0400

    added Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+
+const nav = () => {
+  return (
+    <div className="navbar">
+      <Navbar />
+    </div>
+  );
+}
+
+export default Navbar;
